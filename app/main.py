81ca12c17ddcb4d739f443c380eee19476361394
Bruna Calistrate <bruna.calistrate@gmail.com>
2024-06-10 00:41:36 -0300

    fix: adjust plot commit graph to return graph bytes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from datetime import date, timedelta
+from io import BytesIO
 
-from fastapi import APIRouter, FastAPI, status
+from fastapi import APIRouter, BackgroundTasks, FastAPI, Response, status
 
 from app.commits_graph import CommitsGraph
 from app.github_scraper import GithubScraper
@@ -59,15 +60,22 @@ def create_app() -> FastAPI:
     @app_router.post(
         "/plot_commit_graph/",
         description="Plot commit graph of users.",
-        status_code=status.HTTP_200_OK,
+        status_code=status.HTTP_201_CREATED,
     )
-    def plot_commit_graph(commit_data_payload: CollectCommitDataPayload):
+    async def plot_commit_graph(commit_data_payload: CollectCommitDataPayload):
+        background_tasks = BackgroundTasks()
+        graph = BytesIO()
         cg = CommitsGraph(
             users_list=commit_data_payload.users_list,
             filter_date=commit_data_payload.filter_date,
         )
-        commit_graph_plot = cg.create_commit_graph()
-        return commit_graph_plot
+        commit_graph_fig = cg.create_commit_graph()
+        commit_graph_fig.savefig(graph, format="png", bbox_inches="tight")
+
+        graph_contents: bytes = graph.getvalue()
+        background_tasks.add_task(graph.close)
+        headers = {"Content-Disposition": "inline; filename='out.png'"}
+        return Response(graph_contents, headers=headers, media_type="image/png")
 
     app.include_router(app_router)
 
