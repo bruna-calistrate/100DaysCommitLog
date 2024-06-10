ce4d00d8d4f508c9e169c30f0f90b9242664a51e
Bruna Calistrate <bruna.calistrate@gmail.com>
2024-06-09 23:56:05 -0300

    fix: fetch daily commits from count_daily_commits, remove data transformation; return matplotly figure on create commit_graph

--- app/commits_graph.py
+++ app/commits_graph.py
@@ -1,7 +1,8 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-from utils import get_first_name
+from app.github_scraper import GithubScraper
 
 
 class CommitsGraph:
@@ -9,36 +10,20 @@ class CommitsGraph:
         self.users_list = users_list
         self.filter_date = filter_date
 
-    def create_commit_dataframe(self):
-        from app.github_scraper import GithubScraper
-
+    def create_commits_dataframe(self):
         scraper = GithubScraper(
             users_list=self.users_list, filter_date=self.filter_date
         )
-        commit_data = scraper.collect_users_data()
-        df = pd.DataFrame.from_records(commit_data)
+        daily_commits = scraper.count_daily_commits()
+        df = pd.DataFrame.from_dict(daily_commits).sort_index()
+        df = df.reindex(sorted(df.columns), axis=1)
         return df
 
-    def transform_data(self):
-        df = self.create_commit_dataframe()
-        df["commit_created_at"] = pd.to_datetime(
-            df["commit_created_at"]
-        ).dt.tz_convert("America/Sao_Paulo")
-        df["commit_date"] = df["commit_created_at"].dt.date
-        df["commit_author_first_name"] = df["commit_author"].apply(
-            get_first_name
-        )
-        commits_graph_df = pd.pivot_table(
-            df,
-            index="commit_author_first_name",
-            columns="commit_date",
-            values="commit_sha",
-            aggfunc="count",
-        )
-        return commits_graph_df
-
     def create_commit_graph(self):
-        commits_graph_data = self.transform_data()
+        fig, ax_ = plt.subplots(figsize=(5, 5))
+
+        commits_graph_data = self.create_commits_dataframe()
         cmap = sns.color_palette("blend:#9be9a8,#216e39", as_cmap=True)
         ax = sns.heatmap(commits_graph_data, cmap=cmap, cbar=False)
-        return ax
+        ax.set(xlabel="", ylabel="")
+        return fig
